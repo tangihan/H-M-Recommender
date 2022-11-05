84f0364b78d3777b0d6b0c410bea073b51aa42ed
tangihan <78784964+tangihan@users.noreply.github.com>
2022-11-05 11:21:49 +0800

    added venv environment

---
+++ backend/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\gi han\onedrive - singapore management university\documents\github\h-m-recommender\backend\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
